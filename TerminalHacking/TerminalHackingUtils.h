320d96acc07795f58703d0030265f9d259562184
dicemaster <dicemaster555@gmail.com>
2018-02-01 21:25:28 +0000

    Refactored a few functions, added some extra comments and created a new file that contains all of the game functionalities and now the main.cpp only runs TheGame function.

--- TerminalHacking/TerminalHackingUtils.h
+++ TerminalHacking/TerminalHackingUtils.h
@@ -1,15 +1,14 @@
 #pragma once
 #include "stdafx.h"
 
+//The game function that will run the game in the console in main.cpp
+int TheGame();
+
 //Converts a string to upper 
 void MakeWordUppercase(std::string& word);
 
+bool CheeckIfWordIsCorrect(std::string userInput, std::string secret, bool running, int tries);
 
 int CheckCorrrectLetters(std::string& word, std::string& secret);
 
 int StreamlineOfRandomChars();
-
-int RandInt(int A, int B)
-{
-	return (rand() % B + A);
-}
