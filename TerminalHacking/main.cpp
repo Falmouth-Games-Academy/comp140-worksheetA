54d7e432ad1a9b874a0548661cf283478e371f29
Joachim Rayski <joachim.r.4k@gmail.com>
2018-12-13 16:12:02 +0000

    Did the thong

--- TerminalHacking/main.cpp
+++ TerminalHacking/main.cpp
@@ -5,40 +5,94 @@
 
 const int wordLength = 5;
 const int numberOfWords = 15;
+int numberOfLives = 4;
+std::string guessedWord;
+bool guessed = false;
+
+std::set<std::string> options;
+std::string secret;
 
 int main()
 {
-	// Seed the random number generator with the current time,
-	// to ensure different results each time the program is run
-	srand(static_cast<unsigned int>(time(nullptr)));
-
-	// Initialise word list
-	WordList words(wordLength);
-
-	// Choose secret word
-	std::string secret = words.getRandomWord();
-
-	// Create a set to hold the list of options
-	std::set<std::string> options;
-
-	// Put the secret word in the set
-	options.insert(secret);
-
-	// Fill the set with more words
-	// Using a set for options guarantees that the elements are all different
-	while (options.size() < numberOfWords)
+	if (!guessed)
 	{
-		std::string word = words.getRandomWord();
-		options.insert(word);
-	}
+		// Seed the random number generator with the current time,
+		// to ensure different results each time the program is run
+		srand(static_cast<unsigned int>(time(nullptr)));
 
-	// Display all words
-	for each (std::string word in options)
-	{
-		std::cout << word << std::endl;
+		// Initialise word list
+		WordList words(wordLength);
+
+		// Choose secret word
+		secret = words.getRandomWord();
+
+		// Reset the set that holds the list of options
+		options.clear();
+
+		// Put the secret word in the set
+		options.insert(secret);
+
+		// Fill the set with more words
+		// Using a set for options guarantees that the elements are all different
+		while (options.size() < numberOfWords)
+		{
+			std::string word = words.getRandomWord();
+			options.insert(word);
+		}
+
+		// Display all words
+		for each (std::string word in options)
+		{
+			std::cout << word << std::endl;
+		}
 	}
 
 	// TODO: implement the rest of the game
+
+	std::cin >> guessedWord;
+
+	for each (std::string word in options)
+	{
+		if (guessedWord == word)
+		{
+			if (guessedWord == secret)
+			{
+				std::cout << "Y O U   W I N" << std::endl;
+				guessed = false;
+				numberOfLives = 4;
+				main();
+				return 0;
+			}
+			else if(numberOfLives > 1)
+			{
+				int likeness = 0;
+				for (int i = 0; i<guessedWord.length(); ++i)
+				{
+					if (guessedWord.at(i) == secret.at(i))
+					{
+						likeness += 1;
+					}
+				}
+				std::cout << "Likeness: " << likeness << std::endl;
+				guessed = true;
+				numberOfLives -= 1;
+				main();
+				return 0;
+			}
+			else
+			{
+				std::cout << "G A M E	O V E R" << std::endl;
+				guessed = false;
+				numberOfLives = 4;
+				main();
+				return 0;
+			}
+		}
+	}
+
+	std::cout << "Invalid Guess" << std::endl;
+	guessed = true;
+	main();
 
 	return 0;
 }
