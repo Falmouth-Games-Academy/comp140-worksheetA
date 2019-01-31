39acfc921600818f3c3ee69e591aafe65d59a1a7
Josh Grillo <Josh@Grillo.co.uk>
2019-01-31 10:09:59 +0000

    Code Tidying and comments added

--- TerminalHacking/main.cpp
+++ TerminalHacking/main.cpp
@@ -12,8 +12,9 @@ const int numberOfWords = 15;
 int Calculate_Likeness(std::string secretWord, std::string guessWord)
 {
 	int likeness = 0;
-	for (int i = 0; i < wordLength; i++) 
-		secretWord[i] == guessWord[i] ? likeness++ : true; // if the letters match, increase likeness. Else do nothing
+	for (int i = 0; i < wordLength; i++)  // for all letters...
+		if (secretWord[i] == guessWord[i]) 
+			likeness++; // if the letters match, increase likeness. 
 	return likeness;
 }
 
@@ -38,6 +39,7 @@ int main()
 	// Fill the set with more words
 	// Using a set for options guarantees that the elements are all different
 
+	// map used to store likeness against how many words of that likeness are already added.
 	std::map<int, int> word_tally = { {0,0},{1,0},{2,0},{3,0},{4,0} };
 
 	while (options.size() < numberOfWords)
@@ -46,7 +48,7 @@ int main()
 		int likeness = Calculate_Likeness(secret, word); // gets the likeness between word and likeness
 		if (word_tally[likeness] < 4) { // if there are more 4 words of the same likeness already, dont add this word.
 			options.insert(word);
-			word_tally[likeness] = word_tally[likeness] + 1;
+			word_tally[likeness] = word_tally[likeness] + 1; // record that a word of this likeness has been added.
 		}
 	}
 
@@ -61,25 +63,24 @@ int main()
 	bool valid;
 	bool success = false;
 
-	do {
+	do { // do while used because the player has to iterate through it at least once
 		valid = false;
 		while (!valid) { // get input until guess is valid
 			std::cout << "Enter your Guess..." << std::endl;
 			std::getline(std::cin, guess);
 			std::transform(guess.begin(), guess.end(), guess.begin(), ::toupper); // automatically converts guess to upper case - taken from online
-			for (auto i : options)
-				if (i == guess ) valid = true; // if guess is equal to one of the options, valid becomes true; else is stays false.
+			for (auto i : options) //go through all values in options...
+				if (i == guess) valid = true; // if guess is equal to one of the options, valid becomes true; else is stays false.
 			if (!valid) std::cout << "Invalid guess" << std::endl;
 		}	
-		if (guess == secret) {
+		if (guess == secret)
 			success = true;
-		}
 		else {
 			std::cout << "Likeness between guess and secret word is " << Calculate_Likeness(secret, guess) << std::endl;
 			lives--;
 		}
-	} while (lives > 0 && !success); // iterate while player has lives or hasnt guessed correctly
-	success ? std::cout << "You win!" : std::cout << "You lose!";
+	} while (lives > 0 && !success); // iterate while player has lives and hasnt guessed correctly
+	success ? std::cout << "You win!" : std::cout << "You lose!"; // if success is true prints win, if false prints lose
 
 	return 0;
 }
